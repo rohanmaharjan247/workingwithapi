aab575a0f4e03332ef891bfc3c3e55dc9cd46ed1
Rohan Maharjan <smdnoobs24@gmail.com>
2021-03-26 11:15:16 +0545

    changes to environment

--- src/app/weather/services/weather.service.ts
+++ src/app/weather/services/weather.service.ts
@@ -1,37 +1,38 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
   getCurrentWeather() {
-    return this.http.get('/weatherapi/currentweather');
+    return this.http.get(environment.api + '/currentweather');
   }
 
   getHourlyWeather() {
-    return this.http.get('/weatherapi/hourlyweather');
+    return this.http.get(environment.api + '/hourlyweather');
   }
 
   getDailyForecast(lat: number, lon: number) {
-    return this.http.get('/weatherapi/onecall', {
+    return this.http.get(environment.api + '/onecall', {
       params: new HttpParams().set('lat', String(lat)).set('lon', String(lon)),
     });
   }
 
   getCurrentAirPollution(lat: number, lon: number) {
-    return this.http.get('/weatherapi/currentairpollution', {
+    return this.http.get(environment.api + '/currentairpollution', {
       params: new HttpParams().set('lat', String(lat)).set('lon', String(lon)),
     });
   }
 
   getForecastAirPollution(lat: number, lon: number) {
-    return this.http.get('/weatherapi/forecastairpollution', {
+    return this.http.get(environment.api + '/forecastairpollution', {
       params: new HttpParams().set('lat', String(lat)).set('lon', String(lon)),
     });
   }
   getHistoryAirPollution(lat: number, lon: number, start: number, end: number) {
-    return this.http.get('/weatherapi/forecastairpollution', {
+    return this.http.get(environment.api + '/forecastairpollution', {
       params: new HttpParams()
         .set('lat', String(lat))
         .set('lon', String(lon))
