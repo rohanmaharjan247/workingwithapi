d75749d34bad362ae497f735547d3eaf234cb45f
Rohan Maharjan <smdnoobs24@gmail.com>
2021-03-26 11:20:43 +0545

    changed name of dist

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Serve only the static files form the dist directory
-app.use("/", express.static(__dirname + "/dist/DictionaryApi"));
+app.use("/", express.static(__dirname + "/dist/WeatherKtm"));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/DictionaryApi/index.html'));
+res.sendFile(path.join(__dirname+'/dist/WeatherKtm/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
